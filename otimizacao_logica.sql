117cbe22f429f594a4ff85309fce20433bfc0119
Uanderson <andubryanrj@hotmail.com>
2026-04-28 22:48:05 -0300

    Add SQL optimization and security strategies

---
+++ otimizacao_logica.sql
@@ -0,0 +1,8 @@
+## 🚀 Desafio 1: Otimização
+- Implementação de índices para ganho de performance em consultas complexas.
+- Criação de Procedures para automatizar o CRUD.
+
+## 🛡️ Desafio 2: Segurança e Automação
+- Views para abstração de dados e facilidade de relatórios.
+- Triggers para garantir a integridade (auditoria e validação).
+- Gestão de acessos (RBAC) com diferentes níveis de permissão.
